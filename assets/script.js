8bb72afac8ebdad07d5ad610ec25ce2afa801b8f
Kalyn Reid <reidkalyn361@gmail.com>
2021-05-17 22:21:33 -0700

    Fixed header text being seperated. Added placeholder about me section.

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,9 @@
 const yourName = prompt("What is your name?");
 const error = "If you don't enter your name, it will not affect the website's functionality. But for a more personalized experience, please refresh the page and enter your name.";
-
+const userNameEl = document.querySelector("h1");
 
 if(yourName){
-    document
-        .querySelector("body")
-        .innerHTML = "<h1>"+yourName+",</h1>";
+    userNameEl.textContent = yourName + ",";
     } else {
         alert(error);
 }
