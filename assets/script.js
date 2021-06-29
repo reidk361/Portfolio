0cd87f9b081ae5f79711066f8cf23ed7538a1182
Kalyn Reid <reidkalyn361@gmail.com>
2021-06-28 17:20:35 -0700

    Added button functionality.

--- assets/script.js
+++ assets/script.js
@@ -8,3 +8,35 @@ const projectDiv = document.getElementById("example-pages");
 const digitalArtDiv = document.getElementById("digital-art");
 const contactDiv = document.getElementById("contact-info");
 
+function handleAboutMe(){
+    aboutMeDiv.setAttribute("style","display:content");
+    projectDiv.setAttribute("style","display:none");
+    digitalArtDiv.setAttribute("style","display:none");
+    contactDiv.setAttribute("style","display:none");
+}
+
+function handleProjects(){
+    aboutMeDiv.setAttribute("style","display:none");
+    projectDiv.setAttribute("style","display:content");
+    digitalArtDiv.setAttribute("style","display:none");
+    contactDiv.setAttribute("style","display:none");
+}
+
+function handleArts(){
+    aboutMeDiv.setAttribute("style","display:none");
+    projectDiv.setAttribute("style","display:none");
+    digitalArtDiv.setAttribute("style","display:content");
+    contactDiv.setAttribute("style","display:none");
+}
+
+function handleContact(){
+    aboutMeDiv.setAttribute("style","display:none");
+    projectDiv.setAttribute("style","display:none");
+    digitalArtDiv.setAttribute("style","display:none");
+    contactDiv.setAttribute("style","display:content");
+}
+
+aboutMeBtn.addEventListener("click", handleAboutMe);
+projectBtn.addEventListener("click", handleProjects);
+digitalArtBtn.addEventListener("click", handleArts);
+contactBtn.addEventListener("click", handleContact);
